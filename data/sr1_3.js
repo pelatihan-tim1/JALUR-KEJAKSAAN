abbb24dbd55ff8e946c342b23e9b638d16271257
pelatihan-tim1 <159612525+pelatihan-tim1@users.noreply.github.com>
2024-02-17 14:56:37 +0800

    Add files via upload

---
+++ data/sr1_3.js
@@ -0,0 +1,18 @@
+var json_sr1_3 = {
+"type": "FeatureCollection",
+"name": "sr1_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama": "A", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.420256604480116, -8.396732223507792 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nama": "B", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.419390222728865, -8.396294607874227 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nama": "C", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.418820528862, -8.396644968697229 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Nama": "D", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.417300867470246, -8.396715186876705 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Nama": "E", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.415999897143877, -8.395375386676761 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Nama": "F", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.415753755206083, -8.395109294531101 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Nama": "G", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.415215534232772, -8.394569086849121 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Nama": "H", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.414920727827493, -8.394270760304293 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Nama": "I", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.414498483900914, -8.393542099728792 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Nama": "J", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.415203078189435, -8.393016725510563 ] } },
+{ "type": "Feature", "properties": { "id": "11", "Nama": "K", "Foto": null }, "geometry": { "type": "Point", "coordinates": [ 123.41529975723661, -8.392467777210868 ] } }
+]
+}
